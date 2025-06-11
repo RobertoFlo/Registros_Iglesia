1fdd681dcc343bfb9af49771fee5a0b3bb4d6976
RFlore5 <raflores@innovacion.gob.sv>
2025-06-11 11:36:47 -0600

    Refactor PermissionsTableSeeder to create permission groups and associated permissions for 'gasto', 'bautizo', and 'confirma'

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -14,21 +14,67 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $grupo_1 = PermissionGroup::firstOrCreate(['name' => 'boda']);
 
-        // Permisos para el grupo "boda"
-        $permisos = [
+        $grupo_1 = PermissionGroup::firstOrCreate(['name' => 'boda']);
+        $grupo_2 = PermissionGroup::firstOrCreate(['name' => 'gasto']);
+        $grupo_3 = PermissionGroup::firstOrCreate(['name' => 'bautizo']);
+        $grupo_4 = PermissionGroup::firstOrCreate(['name' => 'confirma']);
+
+        // Permisos por grupos
+        $permiso_1 = [
             'boda.create',
             'boda.view',
             'boda.update',
             'boda.delete',
         ];
+        $permiso_2 = [
+            'gasto.create',
+            'gasto.view',
+            'gasto.update',
+            'gasto.delete',
+        ];
+        $permiso_3 = [
+            'bautizo.create',
+            'bautizo.view',
+            'bautizo.update',
+            'bautizo.delete',
+        ];
+        $permiso_4 = [
+            'confirma.create',
+            'confirma.view',
+            'confirma.update',
+            'confirma.delete',
+        ];
 
-        foreach ($permisos as $permiso) {
+        foreach ($permiso_1 as $permiso) {
             Permission::firstOrCreate([
                 'name' => $permiso,
-                'guard_name' => 'api', // Usa 'api' como guard_name
+                'guard_name' => 'api',
                 'group_id' => $grupo_1->id,
+            ]);
+        }
+
+        foreach ($permiso_2 as $permiso) {
+            Permission::firstOrCreate([
+                'name' => $permiso,
+                'guard_name' => 'api',
+                'group_id' => $grupo_2->id,
+            ]);
+        }
+
+        foreach ($permiso_3 as $permiso) {
+            Permission::firstOrCreate([
+                'name' => $permiso,
+                'guard_name' => 'api',
+                'group_id' => $grupo_3->id,
+            ]);
+        }
+
+        foreach ($permiso_3 as $permiso) {
+            Permission::firstOrCreate([
+                'name' => $permiso,
+                'guard_name' => 'api',
+                'group_id' => $grupo_4->id,
             ]);
         }
     }
